9f274adf10a48ede41bca72e49a1b67363ff4a64
viyaa <vyaaarayytgmail.com>
2026-09-14 21:47:46 +0700

    docs add comments javascript funcions

--- JS/script.js
+++ JS/script.js
@@ -2,10 +2,14 @@ const form = document.getElementById("formPendaftaran");
 const syarat = document.getElementById("syarat");
 const btnDaftar = document.getElementById("btnDaftar");
 
+// mengatur tombol daftar agar aktif
+// jika syarat dan ketentuan dicentang
 syarat.addEventListener("change",function () {
     btnDaftar.disabled = !syarat.checked;
 });
 
+// mencegah halaman melakukan reload 
+// saat form disubmit
 form.addEventListener("submit", function(event) {
     event.preventDefault();
 });
